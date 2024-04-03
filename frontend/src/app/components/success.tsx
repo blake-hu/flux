6d8860fdd5b72f21230390feb0aabc214a281b45
jiayi-jenny-zhou <zhou_jiayi05@outlook.com>
2024-04-02 23:05:31 -0500

    pages set up

---
+++ frontend/src/app/components/success.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+
+export default function SecondFactor({back}) {
+    return (
+        <div className="successOutlineBorder">
+            <h1><b>Authentication</b></h1>
+            <h1><b>Successful!</b></h1>
+            <Button onClick ={()=>back()} >Back</Button>
+        </div>
+    );
+  }
