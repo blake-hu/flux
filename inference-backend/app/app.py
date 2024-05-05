6eb4f84c3fe1d1162e49ffc287b3697d10c29be0
Shun Fujita <sfujit123456@gmail.com>
2024-05-05 14:02:03 -0500

    initial video processing

--- inference-backend/app/app.py
+++ inference-backend/app/app.py
@@ -4,6 +4,8 @@ from PIL import Image
 import numpy as np
 import json
 import base64
+import os
+from ..inference import process, calculate, inference
 
 app = Flask(__name__)
 vgg_model = DeepFace.build_model('VGG-Face')
@@ -18,7 +20,7 @@ def generate_embedding():
     image_file = request.files['image']
 
     try:
-        img = Image.open(image_file)
+        img = Image.open(str(image_file))
         img_array = np.array(img).astype(int)
         embedding_objs = DeepFace.represent(
             img_path=img_array, model_name='VGG-Face', enforce_detection=False)
@@ -34,6 +36,28 @@ def generate_embedding():
     encoded_embedding = base64.b64encode(embedding_json.encode('utf-8'))
 
     return jsonify({'encoded_embedding': encoded_embedding.decode('utf-8')})
+
+
+@app.route('/liveness-detection', methods=['POST'])
+def liveness_detection():
+    if 'requestId' not in request.files:
+        return jsonify({'error': 'No requestId'})
+
+    request_id = request.files['requestId']
+    video_path = os.path.join('/video/', request_id, '.mp4')
+    csv_path = os.path.join('/csv/', request_id, '.csv')
+    frames_path = os.path.join('/frames/', request_id)
+
+    try:
+        if os.path.exists(video_path) and os.path.exists(csv_path):
+            frames_times_list = process.split_video(video_path, frames_path)
+            process.crop_frames(frames_times_list, frames_path)
+        else:
+            # One or both files do not exist
+            return jsonify({'error': 'One or both files not found'})
+        
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 
 if __name__ == '__main__':
